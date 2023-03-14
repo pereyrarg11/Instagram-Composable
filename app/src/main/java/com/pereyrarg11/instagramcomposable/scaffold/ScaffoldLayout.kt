f06f0b97a7dc9fca58d8b111e66c6c8776972df4
Gabriel Pereyra R <pereyrarg11@gmail.com>
2023-03-13 21:36:52 -0600

    chore: example of snack bar

--- app/src/main/java/com/pereyrarg11/instagramcomposable/scaffold/ScaffoldLayout.kt
+++ app/src/main/java/com/pereyrarg11/instagramcomposable/scaffold/ScaffoldLayout.kt
@@ -6,40 +6,54 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.More
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 fun ScaffoldScreen() {
-    Scaffold(topBar = { TopAppBarLayout() }) { innerPadding ->
-        Box(modifier = Modifier
-            .padding(innerPadding)
-            .fillMaxWidth())
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            TopAppBarLayout {
+                coroutineScope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar("Haz pulsado $it")
+                }
+            }
+        },
+        scaffoldState = scaffoldState
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+        )
     }
 }
 
 @Composable
-fun TopAppBarLayout() {
+fun TopAppBarLayout(onClickIcon: (String) -> Unit) {
     TopAppBar(
         title = { Text(text = "Top app bar") },
         backgroundColor = Color.Red,
         contentColor = Color.White,
         elevation = 4.dp,
         navigationIcon = {
-            IconButton(onClick = {}) {
+            IconButton(onClick = { onClickIcon("Atrás") }) {
                 Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
             }
         },
         actions = {
-            IconButton(onClick = {}) {
+            IconButton(onClick = { onClickIcon("Buscar") }) {
                 Icon(imageVector = Icons.Filled.Search, contentDescription = "search")
             }
-            IconButton(onClick = {}) {
+            IconButton(onClick = { onClickIcon("Más") }) {
                 Icon(imageVector = Icons.Filled.MoreVert, contentDescription = "more")
             }
         }
